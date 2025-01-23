0e1b85477b208f0176f570f3dae1ae7ff3b0c817
erfanmoulaee <erfanmoulaee1375@gmail.com>
2025-01-23 21:35:51 +0330

    envirment variable

---
+++ src/app/blogs/_components/CategoryList.jsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+async function CategoryList() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/category/list`);
+  const {
+    data: { categories },
+  } = await res.json();
+  return (
+    <div>
+      <ul className="space-y-4">
+        <Link href={`/blogs/`}>همه</Link>
+        {categories.map((category) => {
+          return (
+            <li key={category.id}>
+              <Link href={`/blogs/category/${category.slug}`}>{category.title}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default CategoryList;
