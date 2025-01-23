de283234a14966747a6f02f8fa88654c38669e08
erfanmoulaee <erfanmoulaee1375@gmail.com>
2025-01-23 22:41:13 +0330

    streaming with loading.js

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
-function BlogPage() {
-  return <div>blog list</div>;
+async function BlogPage() {
+  await new Promise((res) => setTimeout(() => res(), 3000));
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/post/list`);
+  const {
+    data: { posts },
+  } = await res.json();
+  return (
+    <div>
+      {posts.map((post) => {
+        return <div>{post.title}</div>;
+      })}
+    </div>
+  );
 }
 
 export default BlogPage;
