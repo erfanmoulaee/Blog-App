33af756d4c56c26213a3051d6a45503b57a2ca7a
erfanmoulaee <erfanmoulaee1375@gmail.com>
2025-01-25 23:12:01 +0330

    Dynamic Route segment

--- src/app/blogs/_components/PostList.jsx
+++ src/app/blogs/_components/PostList.jsx
@@ -5,7 +5,7 @@ import Author from "./Author";
 import PostInteraction from "./PostInteraction";
 
 async function PostList() {
-  await new Promise((res) => setTimeout(() => res(), 3000));
+  // await new Promise((res) => setTimeout(() => res(), 3000));
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/post/list`);
   const {
     data: { posts },
