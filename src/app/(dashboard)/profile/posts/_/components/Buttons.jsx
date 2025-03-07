97bd37f3f4b9052cd9fcf17a92cee8c2154ccd70
erfanmoulaee <erfanmoulaee1375@gmail.com>
2025-03-07 15:44:29 +0330

    delete post

--- src/app/(dashboard)/profile/posts/_/components/Buttons.jsx
+++ src/app/(dashboard)/profile/posts/_/components/Buttons.jsx
@@ -1,7 +1,10 @@
 "use client";
 import ButtonIcon from "@/ui/ButtonIcon";
+import ConfirmDelete from "@/ui/ConfirmDelete";
+import Modal from "@/ui/Modal";
 import { PencilIcon, PlusIcon, TrashIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
+import { useState } from "react";
 
 export function CreatePost() {
   return (
@@ -14,11 +17,35 @@ export function CreatePost() {
   );
 }
 
-export function DeletePost({ id }) {
+export function DeletePost({ post: { _id, postTitle } }) {
+  const [isDeleteOpen, setIsDeleteOpen] = useState(false);
+
+  // useEffect(() => {
+  //   if (state?.message) {
+  //     toast.success(state.message);
+  //     setIsDeleteOpen(false);
+  //   }
+  //   if (state?.error) {
+  //     toast.error(state.error);
+  //   }
+  // }, [state]);
+
   return (
-    <ButtonIcon variant="outline" onClick={() => console.log(id)}>
-      <TrashIcon className="text--error" />
-    </ButtonIcon>
+    <>
+      <ButtonIcon variant="outline" onClick={() => setIsDeleteOpen(true)}>
+        <TrashIcon className="text-error" />
+      </ButtonIcon>
+      <Modal title={`حذف ${postTitle}`} open={isDeleteOpen} onClose={() => setIsDeleteOpen(false)}>
+        <ConfirmDelete
+          resourceName={postTitle}
+          onClose={() => setIsDeleteOpen(false)}
+          // onConfirm={deletePost.bind(null, postId)}
+          onConfirm={(e) => {
+            e.preventDefautl();
+          }}
+        />
+      </Modal>
+    </>
   );
 }
 
